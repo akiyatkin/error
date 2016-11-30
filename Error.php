6b05129699275cd76836114cd46a12beba45b926
Anton <a.kiyatkin@itlf.ru>
2016-11-30 08:55:04 +0400

    fix

--- Error.php
+++ Error.php
@@ -1,11 +1,28 @@
 <?php
-
 namespace akiyatkin\error;
+
+use infrajs\access\Access;
 
 class Error {
 	public static $conf = array(
 		'test' => false,
 		'debug' => true,
 		'admin' => false
-	)
+	);
+	public static function init(){
+		$conf = Error::$conf;
+		$is = 	($conf['test'] && Access::isTest())||
+				($conf['debug'] && Access::isDebug())||
+				($conf['admin'] && Access::isAdmin());
+		if ($is) {
+			ini_set('error_reporting', E_ALL & ~E_DEPRECATED);
+			ini_set('display_errors', 1);
+			ini_set('display_startup_errors', 1);
+			header('display_errors: true');
+		} else {
+			ini_set('display_errors', -1);
+			ini_set('display_startup_errors', -1);
+			header('display_errors: false');
+		}
+	}
 }
--- infra.php
+++ infra.php
@@ -1,16 +1,4 @@
 <?php
+use akiyatkin\error\Error;
 
-$conf = Config::get('error');
-
-$is = 	($conf['test'] && Access::isTest())||
-		($conf['debug'] && Access::isDebug())||
-		($conf['admin'] && Access::isAdmin());
-
-if ($is) {
-	ini_set('error_reporting', E_ALL);
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-} else {
-	ini_set('display_errors', -1);
-	ini_set('display_startup_errors', -1);
-}
+Error::init();
