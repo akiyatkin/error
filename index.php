1160744134dc56994b7fb819bad95ad7d995180b
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-08-16 20:15:38 +0400

    reverse error list

--- index.php
+++ index.php
@@ -3,14 +3,22 @@ use infrajs\ans\Ans;
 use infrajs\access\Access;
 
 
+
 Access::admin(true);
 
 $ans = array();
 $src = ini_get("error_log");
+
+if (Ans::REQ('clear') && is_file($src)) {
+	file_put_contents($src, '');
+}
+
 if (!is_file($src)) {
 	return Ans::log($ans, 'Файл с логами не найден '.$src);
 }
 echo $src;
+echo '<p><form method="POST"><input type="hidden" name="clear" value="true"><button type="submit">очистить</button></form></p>';
 echo '<pre>';
-$text = file_get_contents($src);
-echo $text;
+$ar = file($src);
+$ar = array_reverse($ar);
+echo implode($ar);
